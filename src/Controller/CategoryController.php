3210d1322fd046506a203d02f23b0d9af0529964
Guillaume DEVAUX (Auxioma) <auxioma.g@gmail.com>
2024-07-18 13:40:54 +0200

    categorie

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ArticlesRepository;
+use App\Repository\CategoryRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class CategoryController extends AbstractController
+{
+    private ArticlesRepository $articlesRepository;
+    private CategoryRepository $categoryRepository;
+
+    public function __construct(ArticlesRepository $articlesRepository, CategoryRepository $categoryRepository)
+    {
+        $this->articlesRepository = $articlesRepository;
+        $this->categoryRepository = $categoryRepository;
+    }
+
+    #[Route('/{category}/{slug}', name: 'app_sous_category')]
+    #[Route('/{slug}', name: 'app_category')]
+    public function index(string $slug, PaginatorInterface $paginator, Request $request): Response
+    {
+        $category = $this->categoryRepository->findOneBySlug($slug);
+        
+        if ($request->attributes->get('_route') === 'app_sous_category') {
+            $categories = $category->getParent()->getSubcategories();
+        } else {
+            $categories = $category->getSubcategories();
+        }
+
+        $articles = $this->articlesRepository->findByCategory($slug);
+
+        $pagination = $paginator->paginate(
+            $articles,
+            $request->query->getInt('page', 1),
+            10
+        );
+
+        return $this->render('category/index.html.twig', [
+            'pagination' => $pagination,
+            'categories' => $categories,
+        ]);
+    }
+}
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,80 +3,37 @@
 namespace App\Controller;
 
 use App\Repository\ArticlesRepository;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpKernel\Attribute\Cache;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    private ArticlesRepository $articlesRepository;
-    private array $breaking;
-    private array $slider;
-    private array $cinema;
-    private array $musique;
-    private array $critiques;
-    private array $evenements;
-    private array $more;
-    private array $theatres;
-    private array $dramas;
-    private array $voyage;
-    private array $tvshows;
-    private array $movies;
-    private array $books;
-    private array $wanderlust;
-    private array $bestof;
-    private array $discover;
-    private array $randomise;
-
-    public function __construct(ArticlesRepository $articlesRepository)
-    {
-        $this->articlesRepository = $articlesRepository;
-
-        // Préchargement des données dans le constructeur
-        $this->breaking = $this->articlesRepository->findBy([], ['modified' => 'DESC'], 4);
-        $this->slider = $this->articlesRepository->findBy([], ['id' => 'DESC'], 4);
-        $this->cinema = $this->articlesRepository->findByCategory('cinema');
-        $this->bestof = $this->articlesRepository->findBy([], ['visit' => 'DESC'], 4);
-        $this->discover = $this->articlesRepository->findBy([], ['visit' => 'ASC'], 10);
-        $this->randomise = $this->articlesRepository->RandomArticles();
-        
-        $this->musique = $this->articlesRepository->findByCategory('actualites-musique');
-        $this->critiques = $this->articlesRepository->findByCategory('critiques-musique');
-        $this->evenements = $this->articlesRepository->findByCategory('evenements-musique');
-        $this->more = $this->articlesRepository->findByCategory('manga-anime');
-        $this->theatres = $this->articlesRepository->findByCategory('theatre-scene');
-        $this->dramas = $this->articlesRepository->findByCategory('dramas');
-        $this->voyage = $this->articlesRepository->findByCategory('voyage');
-        $this->tvshows = $this->articlesRepository->findByCategory('tv-shows');
-        $this->movies = $this->articlesRepository->findByCategory('movies');
-        $this->books = $this->articlesRepository->findByCategory('books');
-        $this->wanderlust = $this->articlesRepository->findByCategory('wanderlust');
-
-    }
-
+    public function __construct(
+        private readonly ArticlesRepository $article
+    ){}
     #[Route('/', name: 'app_home')]
-    #[Cache(smaxage: 3600, public: true)]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
-            'breaking'   => $this->breaking,
-            'slider'     => $this->slider,
-            'cinema'     => $this->cinema,
-            'musique'    => $this->musique,
-            'critiques'  => $this->critiques,
-            'evenements' => $this->evenements,
-            'more'       => $this->more,
-            'theatres'   => $this->theatres,
-            'dramas'     => $this->dramas,
-            'voyage'     => $this->voyage,
-            'tvshows'    => $this->tvshows,
-            'movies'     => $this->movies,
-            'books'      => $this->books,
-            'wanderlust' => $this->wanderlust,
-            'bestof'     => $this->bestof,
-            'discover'   => $this->discover,
-            'randomise'  => $this->randomise,
-        ]);
+            'breaking' => $this->article->findBy([], ['modified' => 'DESC'], 4),
+            'slider'   => $this->article->findBy([], ['id' => 'DESC'], 4),
+            'cinema'   => $this->article->findByCategory('cinema'), 
+            'musique'  => $this->article->findByCategory('actualites-musique'),
+            'critiques' => $this->article->findByCategory('critiques-musique'),
+            'evenements' => $this->article->findByCategory('evenements-musique'),
+            'more' => $this->article->findByCategory('manga-anime'),
+            'theatres' => $this->article->findByCategory('theatre-scene'),
+            'dramas' => $this->article->findByCategory('dramas'),
+            'voyage' => $this->article->findByCategory('voyage'),
+            'tvshows' => $this->article->findByCategory('tv-shows'),
+            'movies' => $this->article->findByCategory('movies'),  
+            'books' => $this->article->findByCategory('books'),
+            'wanderlust' => $this->article->findByCategory('wanderlust'),
+            'bestof' => $this->article->findBy([], ['visit' => 'DESC'], 4),
+            'discover' => $this->article->findBy([], ['visit' => 'DESC'], 4),
+            'randomise' => $this->article->RandomArticles(),
+
+        ]); 
     }
 }
