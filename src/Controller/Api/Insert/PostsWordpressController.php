fcdc576dbeb821c9b3817cf3123a0608f6a07ee6
Guillaume DEVAUX (Auxioma) <auxioma.g@gmail.com>
2024-07-19 10:29:16 +0200

    memory limit

--- src/Controller/Api/Insert/PostsWordpressController.php
+++ src/Controller/Api/Insert/PostsWordpressController.php
@@ -10,6 +10,7 @@ use App\Entity\Articles;
 use App\Repository\ArticlesRepository;
 use App\Repository\CategoryRepository;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 
 class PostsWordpressController extends AbstractController
 {
@@ -20,19 +21,20 @@ class PostsWordpressController extends AbstractController
     private ArticlesRepository $articlesRepository;
     private CategoryRepository $categoryRepository;
     private UserRepository $userRepository;
+    private EntityManagerInterface $entityManager;
 
-    public function __construct(HttpClientInterface $client, ArticlesRepository $articlesRepository, CategoryRepository $categoryRepository, UserRepository $userRepository)
+    public function __construct(HttpClientInterface $client, ArticlesRepository $articlesRepository, CategoryRepository $categoryRepository, UserRepository $userRepository, EntityManagerInterface $entityManager)
     {
         $this->client = $client;
         $this->articlesRepository = $articlesRepository;
         $this->categoryRepository = $categoryRepository;
         $this->userRepository = $userRepository;
+        $this->entityManager = $entityManager;
     }
 
     #[Route('/api/insert/post', name: 'api_posts_wordpress')]
     public function insert()
     {
-        // Récupérer toutes les catégories
         $categories = $this->categoryRepository->findAll();
 
         if (!$categories) {
@@ -42,101 +44,102 @@ class PostsWordpressController extends AbstractController
         $posts = [];
         foreach ($categories as $category) {
             $categoryId = $category->getId();
+            $page = 1;
 
-            // Construire l'URL de l'API avec les paramètres nécessaires
-            $url = self::WORDPRESS_API_URL . '?categories=' . $categoryId . '&per_page=' . self::PER_PAGE . '&status=publish';
+            do {
+                $url = self::WORDPRESS_API_URL . '?categories=' . $categoryId . '&per_page=' . self::PER_PAGE . '&page=' . $page . '&status=publish';
 
-            // Effectuer la requête GET vers l'API WordPress
-            $response = $this->client->request('GET', $url);
+                $response = $this->client->request('GET', $url);
 
-            // Vérifier si la requête a réussi
-            if ($response->getStatusCode() !== 200) {
-                return new JsonResponse(['error' => 'Failed to fetch posts from WordPress for category ' . $categoryId], $response->getStatusCode());
-            }
-
-            // Décoder la réponse JSON
-            $categoryPosts = $response->toArray();
-
-            // Insérer les articles dans la base de données
-            foreach ($categoryPosts as $postData) {
-                // Vérifier si le statut de l'article est "publish"
-                if ($postData['status'] !== 'publish') {
-                    continue;
+                if ($response->getStatusCode() !== 200) {
+                    return new JsonResponse(['error' => 'Failed to fetch posts from WordPress for category ' . $categoryId], $response->getStatusCode());
                 }
 
-                // Vérifier si l'article existe déjà par son identifiant
-                $existingArticle = $this->articlesRepository->find($postData['id']);
+                $categoryPosts = $response->toArray();
 
-                if ($existingArticle) {
-                    // Mettre à jour l'article existant si nécessaire
-                    $existingArticle->setTitle($postData['title']['rendered'] ?? 'No title');
-                    $existingArticle->setSlug($postData['slug'] ?? '');
-                    $existingArticle->setDate(new \DateTime($postData['date'] ?? 'now'));
-                    $existingArticle->setModified(new \DateTime($postData['modified'] ?? 'now'));
-                    $existingArticle->setContent($postData['content']['rendered'] ?? '');
+                if (empty($categoryPosts)) {
+                    break;
+                }
 
-                    // Supprimer et réinsérer les catégories (si besoin)
-                    $existingArticle->clearCategories();
-
-                    foreach ($postData['categories'] ?? [] as $catId) {
-                        $category = $this->categoryRepository->find($catId);
-                        if ($category) {
-                            $existingArticle->addCategory($category);
-                        }
+                foreach ($categoryPosts as $postData) {
+                    if ($postData['status'] !== 'publish') {
+                        continue;
                     }
 
-                    // Mettre à jour l'utilisateur associé
-                    $userId = $postData['author'] ?? null;
-                    if ($userId) {
-                        $user = $this->userRepository->find($userId);
-                        if ($user) {
-                            $existingArticle->setUser($user);
-                        }
-                    }
+                    $existingArticle = $this->articlesRepository->findOneBy(['externalId' => $postData['id']]);
 
-                    // Persister les modifications
-                    try {
-                        $this->articlesRepository->save($existingArticle);
+                    if ($existingArticle) {
+                        $this->updateArticle($existingArticle, $postData);
                         $posts[] = $existingArticle;
-                    } catch (\Exception $e) {
-                        return new JsonResponse(['error' => 'Failed to update article: ' . $e->getMessage()], 500);
-                    }
-                } else {
-                    // Créer un nouvel article si aucun n'existe avec cet ID
-                    $article = new Articles();
-                    $article->setId($postData['id']); // Définir l'ID de manière explicite si nécessaire
-                    $article->setTitle($postData['title']['rendered'] ?? 'No title');
-                    $article->setSlug($postData['slug'] ?? '');
-                    $article->setDate(new \DateTime($postData['date'] ?? 'now'));
-                    $article->setModified(new \DateTime($postData['modified'] ?? 'now'));
-                    $article->setContent($postData['content']['rendered'] ?? '');
-
-                    foreach ($postData['categories'] ?? [] as $catId) {
-                        $category = $this->categoryRepository->find($catId);
-                        if ($category) {
-                            $article->addCategory($category);
-                        }
-                    }
-
-                    $userId = $postData['author'] ?? null;
-                    if ($userId) {
-                        $user = $this->userRepository->find($userId);
-                        if ($user) {
-                            $article->setUser($user);
-                        }
-                    }
-
-                    // Persister le nouvel article
-                    try {
-                        $this->articlesRepository->save($article);
+                    } else {
+                        $article = $this->createArticle($postData);
                         $posts[] = $article;
-                    } catch (\Exception $e) {
-                        return new JsonResponse(['error' => 'Failed to save new article: ' . $e->getMessage()], 500);
                     }
                 }
+
+                $page++;
+            } while (count($categoryPosts) === self::PER_PAGE);
+        }
+
+        $this->entityManager->flush();
+
+        return new JsonResponse(['message' => 'Articles successfully inserted or updated', 'articles' => $posts]);
+    }
+
+    private function updateArticle(Articles $article, array $postData)
+    {
+        $article->setTitle($postData['title']['rendered'] ?? 'No title');
+        $article->setSlug($postData['slug'] ?? '');
+        $article->setDate(new \DateTime($postData['date'] ?? 'now'));
+        $article->setModified(new \DateTime($postData['modified'] ?? 'now'));
+        $article->setContent($postData['content']['rendered'] ?? '');
+        $article->clearCategories();
+
+        foreach ($postData['categories'] ?? [] as $catId) {
+            $category = $this->categoryRepository->find($catId);
+            if ($category) {
+                $article->addCategory($category);
             }
         }
 
-        return new JsonResponse(['message' => 'Articles successfully inserted or updated', 'articles' => $posts]);
+        $userId = $postData['author'] ?? null;
+        if ($userId) {
+            $user = $this->userRepository->find($userId);
+            if ($user) {
+                $article->setUser($user);
+            }
+        }
+
+        $this->entityManager->persist($article);
+    }
+
+    private function createArticle(array $postData): Articles
+    {
+        $article = new Articles();
+        $article->setId($postData['id']); // Assuming 'externalId' is a field to store the WP ID
+        $article->setTitle($postData['title']['rendered'] ?? 'No title');
+        $article->setSlug($postData['slug'] ?? '');
+        $article->setDate(new \DateTime($postData['date'] ?? 'now'));
+        $article->setModified(new \DateTime($postData['modified'] ?? 'now'));
+        $article->setContent($postData['content']['rendered'] ?? '');
+
+        foreach ($postData['categories'] ?? [] as $catId) {
+            $category = $this->categoryRepository->find($catId);
+            if ($category) {
+                $article->addCategory($category);
+            }
+        }
+
+        $userId = $postData['author'] ?? null;
+        if ($userId) {
+            $user = $this->userRepository->find($userId);
+            if ($user) {
+                $article->setUser($user);
+            }
+        }
+
+        $this->entityManager->persist($article);
+
+        return $article;
     }
 }
