a6ec239c5eaa8deff667969093591578e2add203
Guillaume DEVAUX (Auxioma) <auxioma.g@gmail.com>
2024-07-25 12:29:01 +0200

    maj

--- src/Controller/LegalController.php
+++ src/Controller/LegalController.php
@@ -8,11 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class LegalController extends AbstractController
 {
-<<<<<<< HEAD
     #[Route('/o/o/o/legal', name: 'legal', priority: 1)]
-=======
-    #[Route('/legal', name: 'legal', priority: 1)]
->>>>>>> 9aa4e7fdbbe9b9961afc87c5e8886015aea83af5
     public function index(): Response
     {
         return $this->render('legal/pricacy-policy.html.twig');
