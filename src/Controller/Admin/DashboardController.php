d05b9fac056f0e171486129c3c920af600100020
Guillaume DEVAUX (Auxioma) <auxioma.g@gmail.com>
2024-09-23 19:03:39 +0200

    google

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -9,13 +9,33 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Service\GoogleSearchConsoleService;
 
 class DashboardController extends AbstractDashboardController
 {
+    private $searchConsoleService;
+
+    // Injecter le service GoogleSearchConsoleService
+    public function __construct(GoogleSearchConsoleService $searchConsoleService)
+    {
+        $this->searchConsoleService = $searchConsoleService;
+    }
+
     #[Route('/admin_olga150187', name: 'admin', priority: 10)]
     public function index(): Response
     {
-        return $this->render('bundles/EasyAdminBundle/page/login.html.twig');
+        // Utiliser le service pour obtenir les données Google Search Console
+        $siteUrl = 'https://justfocus.info'; // Remplace par ton URL
+        $startDate = '2023-01-01';
+        $endDate = '2024-09-31';
+
+        // Récupérer les données via l'API
+        $searchAnalytics = $this->searchConsoleService->getSearchAnalytics($siteUrl, $startDate, $endDate);
+dd($searchAnalytics);
+        // Passer les données à la vue
+        return $this->render('admin/dashboard.html.twig', [
+            'searchAnalytics' => $searchAnalytics,
+        ]);
     }
 
     public function configureDashboard(): Dashboard
