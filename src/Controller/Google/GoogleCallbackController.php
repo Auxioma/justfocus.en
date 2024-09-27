d78c4a2640c7538c34d270a72efb10109e667b57
Guillaume DEVAUX (Auxioma) <auxioma.g@gmail.com>
2024-09-27 19:38:13 +0200

    google

---
+++ src/Controller/Google/GoogleCallbackController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller\Google;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Service\GoogleSearchConsoleService;
+use Symfony\Component\Routing\Attribute\Route;
+
+class GoogleCallbackController extends AbstractController
+{
+    #[Route('/api/google/cloud/callback', name: 'google_callback')]
+    public function callback(Request $request, GoogleSearchConsoleService $googleService): Response
+    {
+        // Récupérer le code dans l'URL de callback
+        $code = $request->get('code');
+        
+        if ($code) {
+            // Échanger le code contre un token d'accès
+            $accessToken = $googleService->getClient()->fetchAccessTokenWithAuthCode($code);
+
+            // Sauvegarder le token pour les futures requêtes
+            file_put_contents($this->getParameter('kernel.project_dir') . '/config/google/token.json', json_encode($accessToken));
+
+            return $this->redirectToRoute('admin'); // Rediriger vers la page admin ou une autre page de votre choix
+        }
+
+        return new Response('Erreur lors de l\'authentification.', Response::HTTP_FORBIDDEN);
+    }
+
+}
--- src/Service/GoogleSearchConsoleService.php
+++ src/Service/GoogleSearchConsoleService.php
@@ -6,21 +6,29 @@ namespace App\Service;
 use Google\Client;
 use Google\Service\SearchConsole;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class GoogleSearchConsoleService
 {
     private $client;
     private $searchConsole;
     private $params;
+    private $requestStack;
 
-    public function __construct(ParameterBagInterface $params)
+    public function __construct(ParameterBagInterface $params, RequestStack $requestStack)
     {
         $this->params = $params;
+        $this->requestStack = $requestStack;
 
         // Initialisation du client Google
         $this->client = new Client();
         $this->client->setAuthConfig($this->params->get('kernel.project_dir') . '/config/google/credentials.json');
         $this->client->addScope(SearchConsole::WEBMASTERS_READONLY);
+
+        // Définir l'URI de redirection
+        $currentRequest = $this->requestStack->getCurrentRequest();
+        $redirectUri = $currentRequest->getSchemeAndHttpHost() . '/callback';  // par ex., http://localhost:8000/callback
+        $this->client->setRedirectUri($redirectUri);
 
         // Vérification si un token d'accès existe déjà
         $tokenPath = $this->params->get('kernel.project_dir') . '/config/google/token.json';
@@ -36,7 +44,7 @@ class GoogleSearchConsoleService
             } else {
                 // Rediriger l'utilisateur pour obtenir un nouveau token
                 $authUrl = $this->client->createAuthUrl();
-                echo 'Ouvrez ce lien dans votre navigateur pour authentifier : ' . $authUrl;
+                header('Location: ' . filter_var($authUrl, FILTER_SANITIZE_URL));
                 exit;
             }
 
@@ -62,9 +70,4 @@ class GoogleSearchConsoleService
         ]);
     }
 }
-
-
-
-
-
 
