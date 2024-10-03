ee09c83372b85fe7727960043d16e8bf3f61cea8
Guillaume DEVAUX (Auxioma) <auxioma.g@gmail.com>
2024-10-03 22:49:09 +0200

    gm

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -23,7 +23,7 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin_olga150187', name: 'admin', priority: 10)]
     public function index(): Response
     {
-        try {
+        /*try {
             // Essayer de récupérer les données de Google Search Console
             $searchConsoleData = $this->googleService->getSearchConsoleData();
 
@@ -34,9 +34,7 @@ class DashboardController extends AbstractDashboardController
             }
 
             throw $e; // Lancer d'autres types d'erreurs
-        }
-
-        dd($searchConsoleData);
+        }*/
 
         // Si tout est bon, afficher les données
         return $this->render('bundles/EasyAdminBundle/page/login.html.twig', [
