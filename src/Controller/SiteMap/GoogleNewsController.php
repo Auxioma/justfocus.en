063ca15d19fde3e8de2745c00fd22eb3928578d1
Guillaume DEVAUX (Auxioma) <auxioma.g@gmail.com>
2024-10-05 13:50:27 +0200

    bing

--- src/Controller/SiteMap/GoogleNewsController.php
+++ src/Controller/SiteMap/GoogleNewsController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller\SiteMap;
 
-use App\Entity\Articles;
 use App\Repository\ArticlesRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
