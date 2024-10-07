d7cc4b4db760aad8506dd0ac59421108967e2397
Guillaume DEVAUX (Auxioma) <auxioma.g@gmail.com>
2024-10-07 20:47:32 +0200

    purgecss

---
+++ purgecss.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./templates/**/*.html.twig", "./assets/**/*.js"],
+  css: ["./public/assets/css/*.css"],
+  output: "./public/assets/css/",
+  safelist: [],
+  rejected: true,
+};
