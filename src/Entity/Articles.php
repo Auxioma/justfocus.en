e2f8c2be8f6d96ea9f1cf42a7a47607f2c007d9d
Guillaume DEVAUX (Auxioma) <auxioma.g@gmail.com>
2024-10-08 18:31:27 +0200

    commentaire entity

--- src/Entity/Articles.php
+++ src/Entity/Articles.php
@@ -66,11 +66,18 @@ class Articles
     #[ORM\Column(type: Types::INTEGER)]
     private int $likes = 0;
 
+    /**
+     * @var Collection<int, Comment>
+     */
+    #[ORM\OneToMany(targetEntity: Comment::class, mappedBy: 'article')]
+    private Collection $comments;
+
     public function __construct()
     {
         $this->categories = new ArrayCollection();
         $this->media = new ArrayCollection();
         $this->tags = new ArrayCollection();
+        $this->comments = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -315,6 +322,36 @@ class Articles
     public function incrementLikes(): self
     {
         ++$this->likes;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Comment>
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): static
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments->add($comment);
+            $comment->setArticle($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): static
+    {
+        if ($this->comments->removeElement($comment)) {
+            // set the owning side to null (unless already changed)
+            if ($comment->getArticle() === $this) {
+                $comment->setArticle(null);
+            }
+        }
 
         return $this;
     }
